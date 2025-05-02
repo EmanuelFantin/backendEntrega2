84b9a8edca20e6fc10e8ae418d46624f7f5ebcdc
Emanuel Fantin <101533635+EmanuelFantin@users.noreply.github.com>
2025-05-02 17:08:21 -0300

    Add files via upload

--- public/js/realtime.js
+++ public/js/realtime.js
@@ -5,6 +5,9 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   const data = Object.fromEntries(new FormData(form));
   socket.emit('nuevoProducto', data);
+
+  // vaciar el formulario visualmente
+  form.reset();
 });
 
 const deleteForm = document.getElementById('deleteForm');
@@ -12,9 +15,28 @@ deleteForm.addEventListener('submit', e => {
   e.preventDefault();
   const { id } = Object.fromEntries(new FormData(deleteForm));
   socket.emit('eliminarProducto', id);
+
+  // vaciar el campo de ID
+  deleteForm.reset();
 });
 
 socket.on('productosActualizados', products => {
   const ul = document.getElementById('productList');
-  ul.innerHTML = products.map(p => `<li>${p.title} - $${p.price}</li>`).join('');
+  ul.innerHTML = '';
+  products.forEach(p => {
+    const li = document.createElement('li');
+    li.innerHTML = `${p.title} - $${p.price} <button onclick="deleteProduct('${p.id}')">Eliminar</button>`;
+    ul.appendChild(li);
+  });
+
+  const msg = document.createElement('p');
+  msg.innerText = 'Lista actualizada';
+  msg.style.color = 'green';
+  document.body.appendChild(msg);
+  setTimeout(() => msg.remove(), 2000);
 });
+
+//funcion de eliminar
+function deleteProduct(id) {
+  socket.emit('eliminarProducto', id);
+}
